639a251a5704c4e81c417f6e809bdc342e245664
wkr1125 <1125347922@qq.com>
2023-01-04 10:09:46 +0800

    exception handler

--- test-consumer/src/main/java/com/example/testprovider/controller/ConsumerController.java
+++ test-consumer/src/main/java/com/example/testprovider/controller/ConsumerController.java
@@ -4,9 +4,12 @@ import com.example.testprovider.service.ConsumerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author wenkr
@@ -19,6 +22,8 @@ import java.util.Map;
 @RequestMapping("/consumer")
 public class ConsumerController {
 
+    private static final String UPLOAD_PATH = "C:/Temp/";
+
     @Autowired
     ConsumerService consumerService;
 
@@ -26,7 +31,7 @@ public class ConsumerController {
     RestTemplate restTemplate;
 
     @GetMapping("/getRequest")
-    public String getRequest(){
+    public String getRequest() {
         return "ConsumerController : " + restTemplate.getForObject("http://localhost:8081/provider/getRequest", String.class);
     }
 
@@ -45,6 +50,36 @@ public class ConsumerController {
         map.put("age", 3);
         restTemplate.put("http://localhost:8081/provider/putRequest", map, String.class);
         return "putRequest";
+    }
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) throws Exception {
+        //不可以上传空文件
+        if (file.isEmpty()) {
+            return "文件为空，请选择文件后再上传";
+        }
+
+        String filename = file.getOriginalFilename();
+        String suffix = filename.substring(filename.lastIndexOf("."));
+        if (!".jpg".equals(suffix)) {
+            return "上传失败，文件类型非jpg";
+        }
+
+        filename = UPLOAD_PATH + filename;
+        File dest = new File(filename);
+
+        //若文件已存在则不执行保存操作
+        try {
+            if (!dest.exists()) {
+                file.transferTo(dest);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "上传失败";
+        }
+
+        return "上传成功";
+
     }
 
 
---
+++ test-consumer/src/test/java/com/example/testprovider/controller/ConsumerControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.testprovider.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author wenkr
+ * @version V1.0
+ * @Package com.example.testprovider.controller
+ * @date 2023/1/3 10:45
+ * @Copyright ©
+ */
+class ConsumerControllerTest {
+
+    @Test
+    void getRequest() {
+    }
+
+    @Test
+    void postRequest() {
+    }
+
+    @Test
+    void putRequest() {
+    }
+}
